bd9859b2e224fe171e156874fc6af968694e66d0
Mark Vander Stel <mvndrstl@gmail.com>
2016-08-18 17:14:53 -0400

    Add code to remove git plugins when no git

--- install.sh
+++ install.sh
@@ -4,19 +4,20 @@ ln -f ./.bashrc ~/.bashrc
 ln -f ./.vimrc ~/.vimrc
 
 mkdir -p ~/bin ~/.vim/{autoload,bundle,swap,undo}
-ln -f ./vim-plugins ~/.vim/plugins
 
 # Test for VCS
 if hash git 2>/dev/null; then
-    git="true"
     ln -f ./.gitconfig ~/.gitconfig
 elif hash svn 2>/dev/null; then
-    svn="true"
+    # Remove git plugins, since we don't have git
+    sed '/git/d' ./vim-plugins > ./vim-plugins.tmp
+    mv ./vim-plugins.tmp ./vim-plugins
 else
     echo "No VCS found, no good way to install Vim plugins"
     exit 1
 fi
 
+ln -f ./vim-plugins ~/.vim/plugins
 cd ~/.vim
 # Use pathogen + voom because easy
 [ -e autoload/pathogen.vim ] || curl -LSso autoload/pathogen.vim https://tpo.pe/pathogen.vim
